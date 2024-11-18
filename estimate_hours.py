51994e7714b85177af8b894ca9d000941795bf65
Mintae Kim <mtwow789@gmail.com>
2024-11-18 03:37:12 -0800

    added CORS

--- estimate_hours.py
+++ estimate_hours.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import time
 import random
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Sample condition-based care hours dictionary
 condition_hours = {
